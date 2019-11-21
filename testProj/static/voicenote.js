66df5349f4afab3055b03cb128df4ddb818a623f
cupOJ <almanie@eng.utah.edu>
2019-11-21 13:14:19 -0700

    Changes to voice notes

--- testProj/static/voicenote.js
+++ testProj/static/voicenote.js
@@ -4,7 +4,7 @@ const pauseButton = document.getElementById('pause');
 const resumeButton = document.getElementById('resume');
 const recordings = document.getElementById('recordings');
 const main = document.getElementById('main');
-var canvas = document.querySelector('.visualizer');
+// var canvas = document.querySelector('.visualizer');
 var submitButton = document.getElementById('submit');
 var form = new FormData();
 var balob =new Object();
@@ -21,8 +21,8 @@ const supportsWasm = WebAssembly && typeof WebAssembly.instantiate === 'function
 const supportsUserMediaAPI = navigator.mediaDevices && typeof navigator.mediaDevices.getUserMedia === 'function';
 const isBrowserSupported = supportsWasm && supportsUserMediaAPI;
 
-var audioCtx = new (window.AudioContext || webkitAudioContext)();
-var canvasCtx = canvas.getContext("2d");
+// var audioCtx = new (window.AudioContext || webkitAudioContext)();
+// var canvasCtx = canvas.getContext("2d");
 
 if (isBrowserSupported) {
     window.mp3MediaRecorder
@@ -41,8 +41,8 @@ if (isBrowserSupported) {
                 recorder.onstart = e => {
                     console.log('onstart', e);
                     blobs = [];
-                    visualize(mediaStream);
-                    canvas.style.display = 'block';
+                    // visualize(mediaStream);
+                    // canvas.style.display = 'block';
                     startButton.disabled = true;
                     stopButton.disabled = false;
                     pauseButton.disabled = false;
@@ -77,12 +77,16 @@ if (isBrowserSupported) {
                     console.log('onpause', e);
                     resumeButton.disabled = false;
                     pauseButton.disabled = true;
+                    stopButton.disabled = true;
+
                 };
 
                 recorder.onresume = e => {
                     console.log('onresume', e);
                     resumeButton.disabled = true;
                     pauseButton.disabled = false;
+                    stopButton.disabled = false;
+
                 };
 
                 recorder.onerror = e => {
@@ -96,17 +100,17 @@ if (isBrowserSupported) {
     });
 
     stopButton.addEventListener('click', () => {
-        canvas.style.display = 'none';
+        // canvas.style.display = 'none';
         recorder.stop();
     });
 
     pauseButton.addEventListener('click', () => {
-        canvas.style.display = 'none';
+        // canvas.style.display = 'none';
         recorder.pause();
     });
 
     resumeButton.addEventListener('click', () => {
-        canvas.style.display = 'block';
+        // canvas.style.display = 'block';
         recorder.resume();
     });
 } else {
@@ -132,88 +136,55 @@ if (isBrowserSupported) {
     }
 }
 
-$(document).on('submit', '#post-form', function (e) {
-    // e.preventDefault()
-    console.log("yo")
-    var fd = document.querySelector("form");
-    form = new FormData(fd)
-    form.append('audio', balob);
-    var btn = document.getElementById('submit');
-    btn.innerHTML = "";
-    var spn = document.createElement("SPAN");
-    spn.classList.add("spinner-border", "spinner-border-sm");
-    var text = document.createTextNode(" Sending..");
-    btn.appendChild(spn);
-    btn.appendChild(text);
-    // $.ajax({
-    //     url: "/voicenote/success",
-    //     type: 'POST',
-    //     data: form,
-    //     processData: false,
-    //     contentType: false,
-    //     error: function () {
-    //         $("#myModal").modal();
-    //         btn.innerHTML = "Submit";
-    //     }
-    // }).done(function (data) {
-    //     if (data.success) {
-    //         window.location.href = data.url;
-    //     }
-    // });
-    // var xhr = new XMLHttpRequest();
-    // xhr.open('POST', 'http://127.0.0.1:8000/voicenote/', true);
-    // xhr.send(form);
-});
+// function visualize(stream) {
+//     var source = audioCtx.createMediaStreamSource(stream);
 
-function visualize(stream) {
-    var source = audioCtx.createMediaStreamSource(stream);
+//     var analyser = audioCtx.createAnalyser();
+//     analyser.fftSize = 2048;
+//     var bufferLength = analyser.frequencyBinCount;
+//     var dataArray = new Uint8Array(bufferLength);
 
-    var analyser = audioCtx.createAnalyser();
-    analyser.fftSize = 2048;
-    var bufferLength = analyser.frequencyBinCount;
-    var dataArray = new Uint8Array(bufferLength);
+//     source.connect(analyser);
+//     //analyser.connect(audioCtx.destination);
 
-    source.connect(analyser);
-    //analyser.connect(audioCtx.destination);
+//     draw()
 
-    draw()
+//     function draw() {
+//         WIDTH = canvas.width
+//         HEIGHT = canvas.height;
 
-    function draw() {
-        WIDTH = canvas.width
-        HEIGHT = canvas.height;
+//         requestAnimationFrame(draw);
 
-        requestAnimationFrame(draw);
+//         analyser.getByteTimeDomainData(dataArray);
 
-        analyser.getByteTimeDomainData(dataArray);
+//         canvasCtx.fillStyle = '#74A541';
+//         canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
 
-        canvasCtx.fillStyle = '#74A541';
-        canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+//         canvasCtx.lineWidth = 2;
+//         canvasCtx.strokeStyle = '#F5FBEF';
 
-        canvasCtx.lineWidth = 2;
-        canvasCtx.strokeStyle = '#F5FBEF';
+//         canvasCtx.beginPath();
 
-        canvasCtx.beginPath();
-
-        var sliceWidth = WIDTH * 1.0 / bufferLength;
-        var x = 0;
+//         var sliceWidth = WIDTH * 1.0 / bufferLength;
+//         var x = 0;
 
 
-        for (var i = 0; i < bufferLength; i++) {
+//         for (var i = 0; i < bufferLength; i++) {
 
-            var v = dataArray[i] / 128.0;
-            var y = v * HEIGHT / 2;
+//             var v = dataArray[i] / 128.0;
+//             var y = v * HEIGHT / 2;
 
-            if (i === 0) {
-                canvasCtx.moveTo(x, y);
-            } else {
-                canvasCtx.lineTo(x, y);
-            }
+//             if (i === 0) {
+//                 canvasCtx.moveTo(x, y);
+//             } else {
+//                 canvasCtx.lineTo(x, y);
+//             }
 
-            x += sliceWidth;
-        }
+//             x += sliceWidth;
+//         }
 
-        canvasCtx.lineTo(canvas.width, canvas.height / 2);
-        canvasCtx.stroke();
+//         canvasCtx.lineTo(canvas.width, canvas.height / 2);
+//         canvasCtx.stroke();
 
-    }
-}
+//     }
+// }
